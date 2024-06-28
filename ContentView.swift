00285fb4749698c3cc8558760be271f7f77e7fe0
Павел <38608719+adasprin@users.noreply.github.com>
2024-06-28 15:52:06 +0300

    Update ContentView.swift

--- ContentView.swift
+++ ContentView.swift
@@ -1,10 +1,3 @@
-//
-//  ContentView.swift
-//  navlist_swui
-//
-//  Created by Pavel Ruzmanov on 24.06.2024.
-//
-/*
 import SwiftUI
 
 struct ContentView: View {
@@ -16,66 +9,39 @@ struct ContentView: View {
     Cat(name: "Ualla")
   ]
   func addCat() {
-    cats.append(Cat(name: "A cat"))
+    showingAlert.toggle()
   }
-  var body: some View {
-   NavigationStack {
-   List(cats.indices, id: \.self, editActions: .delete)) { index in
-   NavigationLink(self.cats[index].name){
-   CatDetails(cat: self.cats[index].name)
-   }.swipeActions() {
-   Button("Delete") {
-   print(index)
-   cats.remove(at: index) // Не понимаю как вынуть номер текущего элемента
-   }.tint(.red)
-   }
-   }
-   .navigationTitle("Cats list")
-   .navigationBarTitleDisplayMode(.automatic)
-   .toolbar {
-   Button(action: addCat) {
-   Text("Add a cat")
-   }
-   }
-   }
-   }
-   }
-   
-   #Preview {
-   ContentView()
-   }
-   struct Cat: Identifiable {
-   var name: String
-   var id = UUID()
-   }
-   */
-
-import SwiftUI
-
-struct ContentView: View {
-  @State private var cats = [
-    Cat(name: "Tom"),
-    Cat(name: "Bob"),
-    Cat(name: "Liam"),
-    Cat(name: "Tina"),
-    Cat(name: "Ualla")
-  ]
-  func addCat() {
-    cats.append(Cat(name: "A cat"))
+  func submit() {
+    if (name != "") {
+      cats.append(Cat(name: name))
+      name = ""
+    } else {
+      return
+    }
   }
+  @State private var showingAlert = false
+  @State private var name = ""
+  
   var body: some View {
     NavigationStack {
-        // для того чтобы значения стимились надо указать $ перед экземпляром
-        List($cats, id: \.self, editActions: .delete) { $user in
-          NavigationLink(user.name){
+      // для того чтобы значения стимились надо указать $ перед экземпляром
+      List($cats, id: \.self, editActions: .delete) { $user in
+        NavigationLink(user.name) {
           CatDetails(cat: user.name)
-          }
         }
+      }
       .navigationTitle("Cats list")
       .navigationBarTitleDisplayMode(.automatic)
       .toolbar {
         Button(action: addCat) {
           Text("Add a cat")
+        }.alert(
+          "Имя кота",
+          isPresented: $showingAlert
+        ) {
+          TextField("Enter your name", text: $name)
+          Button("OK", action: submit)
+          Button("Cancel", role: .cancel) {}
         }
       }
     }
